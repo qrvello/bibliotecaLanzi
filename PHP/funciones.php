b4c4bca9a9031e999976c0f610619492d14e4e2f
Santiago Curvello <santicurvello@gmail.com>
2020-10-16 19:24:12 -0300

    mostrar materias andando

--- PHP/funciones.php
+++ PHP/funciones.php
@@ -2,23 +2,30 @@
 
 function conectar()
 {
-    $host = 'localhost';
-    $user = 'santiago';
-    $password = 'riverplate';
-    $database = 'biblioteca';
-    $link = mysqli_connect($host, $user, $password, $database);
+	$host = 'localhost';
+	$user = 'santiago';
+	$password = 'riverplate';
+	$database = 'biblioteca';
+	$link = mysqli_connect($host, $user, $password, $database);
 
-    if (mysqli_connect_errno()) {
-        echo "La conexión a la base de datos falló: " . mysqli_connect_error();
-    } else {
-        return $link;
-    }
+	if (mysqli_connect_errno()) {
+		echo "La conexión a la base de datos falló: " . mysqli_connect_error();
+	} else {
+		return $link;
+	}
 }
 
 function mostrar()
 {
-    $link = conectar();
-    $query = mysqli_query($link, 'SELECT * FROM materias');
-    $materias = mysqli_fetch_assoc($query);
-    return $materias;
+	$link = conectar();
+	$query = mysqli_query($link, 'SELECT * FROM materias');
+
+	while ($materia = mysqli_fetch_assoc($query)) {
+		echo '<tr>';
+		echo '<td>' . $materia['id'] . '</td>';
+		echo '<td>' . $materia['nombre'] . '</td>';
+		echo '<td>' . $materia['año'] . '°</td>';
+		echo '<td>' . $materia['division'] . '°</td>';
+		echo '</tr>';
+	}
 }
